6a527840ccf5d4c7b47f3d047ddbd324feab1485
Colin Johnston <dev@electronspin.com>
2016-03-24 21:49:21 -0700

    tooltip snippet for quick JS check fixed

--- templates/footer.php
+++ templates/footer.php
@@ -3,7 +3,7 @@
     <div class="content row">
       <?php dynamic_sidebar('sidebar-footer'); ?>
       <p>
-      Tooltip for testing that Foundation <span data-tooltip aria-haspopup="true" class="has-tip" data-disable-hover="false" tabindex="1" title="Good old fashioned JavaScript here, transpiled by Babel from ES2015.">JavaScript</span> is working. If you see the <a href="http://foundation.zurb.com/sites/docs/tooltip.html">standard black tooltip</a>, it's working. Remove this from the footer template.
+      Tooltip for testing that Foundation <span data-tooltip aria-haspopup="true" class="has-tip right" data-disable-hover="false" tabindex="1" title="Good old fashioned JavaScript here, transpiled by Babel from ES2015.">JavaScript</span> is working. If you see the <a href="http://foundation.zurb.com/sites/docs/tooltip.html">standard black tooltip</a>, it's working. Remove this from the footer template.
       </p>
     </div>
   </div>
